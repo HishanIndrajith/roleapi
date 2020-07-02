203eb6b0305961f5a78be733ea29b4f7d3184d61
HishanIndrajith <hishan.indrajith.95hia@gmail.com>
2020-07-02 10:56:49 +0530

    ROLE API: Reformated Code

--- src/main/java/com/haulmatic/api/controllers/RoleController.java
+++ src/main/java/com/haulmatic/api/controllers/RoleController.java
@@ -23,19 +23,19 @@ public class RoleController {
         this.roleService = roleService;
     }
 
-    @ApiOperation(value = "Get a role with nic",response = DetailedRole.class)
+    @ApiOperation(value = "Get a role with nic", response = DetailedRole.class)
     @GetMapping("/roles/{nic}")
     public ResponseEntity<DetailedRole> getRole(@PathVariable("nic") String nic) {
         return roleService.getRoleByNic(nic);
     }
 
-    @ApiOperation(value = "Create a new role",response = DetailedRole.class)
+    @ApiOperation(value = "Create a new role", response = DetailedRole.class)
     @PostMapping("/roles")
     public ResponseEntity<DetailedRole> createRole(@RequestBody DetailedRole detailedRole) {
         return roleService.createRole(detailedRole);
     }
 
-    @ApiOperation(value = "Update a role",response = DetailedRole.class)
+    @ApiOperation(value = "Update a role", response = DetailedRole.class)
     @PutMapping("/roles/{nic}")
     public ResponseEntity<DetailedRole> updateRole(@PathVariable("nic") String nic, @RequestBody DetailedRole detailedRole) {
         return roleService.updateRole(nic, detailedRole);
@@ -47,7 +47,7 @@ public class RoleController {
         return roleService.deleteRole(nic);
     }
 
-    @ApiOperation(value = "View a list of available roles by the organization and the role type",response = Iterable.class)
+    @ApiOperation(value = "View a list of available roles by the organization and the role type", response = Iterable.class)
     @GetMapping("/roles")
     public ResponseEntity<List<Role>> getRolesByOrganizationAndRoleType(
             @RequestParam(value = "organization") String organization,
